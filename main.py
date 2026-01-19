9523550c0267ee3152415251e11b8ecd713efd53
Tiag0Sants <mbetani.betania.10@gmail.com>
2026-01-19 09:08:10 -0300

    Add initial implementation of the Ada chatbot system

---
+++ main.py
@@ -0,0 +1,7 @@
+def sistema_ada():
+    nome_bot= "Ada"
+    print ("---------------------------------------------------------")
+    print (f"👩‍💻 {nome_bot} Inicializada...")
+    print (f"{nome_bot}: Olá! Sou a {nome_bot}, sua assistente virtual de T.I")
+    print ("Estou pronta para ajudar. Me diga o que está acontecendo.")
+    print ("---------------------------------------------------------")
